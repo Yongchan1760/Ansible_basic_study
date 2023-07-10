32f7bcd073f856453407d1253895c03547275666
POGLEZZING <ksy1760@naver.com>
2023-07-10 17:51:21 +0900

    ansible test

---
+++ Ansible_study/Ansible_Hello_World.py
@@ -0,0 +1,13 @@
+
+https://wikidocs.net/192510
+
+
+ansible all -m ping -k
+
+192.168.10.9 | SUCCESS => {
+    "ansible_facts" : {
+        "discovered_interpreter_python": "/usr/bin/python3"
+    },
+    "changed": false,
+    "ping": "pong"
+}
